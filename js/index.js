e59219e306256936e587a843c5c6f813c2acc160
PetyaKatsarova <pskpetya@gmail.com>
2024-04-20 13:05:30 +0200

    hamburger menu working

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
-function toggleMenu() {
-    var menuItems = document.getElementById("menuItems");
-    if (menuItems.classList.contains("hidden")) {
-        menuItems.classList.remove("hidden");
-        menuItems.classList.add("show");
-        menuItems.style.display="block"
-    } else {
-        menuItems.classList.add("hidden");
-        menuItems.classList.remove("show");
-    }
-}
+var menuItems = document.getElementById("menuItems");
+var menuIcon  = document.getElementById("menuIcon");
 
-document.getElementsByClassName("mobile-nav-link").forEach(el => {
-    el.addEventListener("click", () => {
-        toggleMenu()
-    })
+menuIcon.addEventListener("click", (e)=> {
+    var menuItems = e.target.nextElementSibling;
+    if (menuItems.style.display === "none" || menuItems.style.display === "") {
+        menuItems.style.display = "block";
+        menuItems.className = "show"
+    } else {
+        menuItems.style.display = "none";
+        menuItems.className = "hidden"
+    }
 })
 
-
+document.querySelector("#menuIcon").addEventListener("click", (e) => {
+    if (e.target.textContent == `x`) {
+        e.target.textContent = `☰`
+    } else {
+        e.target.textContent = `x`
+    }
+})
