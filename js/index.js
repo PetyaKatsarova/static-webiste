0f76c94402b39d0d9aadd079192e8881b455515d
PetyaKatsarova <pskpetya@gmail.com>
2024-04-20 15:33:17 +0200

    work in progress

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var menuItems = document.getElementById("menuItems");
-var menuIcon  = document.getElementById("menuIcon");
+const menuItems = document.getElementById("menuItems");
+const menuIcon  = document.getElementById("menuIcon");
 
 menuIcon.addEventListener("click", (e)=> {
     var menuItems = e.target.nextElementSibling;
@@ -10,12 +10,26 @@ menuIcon.addEventListener("click", (e)=> {
         menuItems.style.display = "none";
         menuItems.className = "hidden"
     }
-})
-
-document.querySelector("#menuIcon").addEventListener("click", (e) => {
     if (e.target.textContent == `x`) {
         e.target.textContent = `☰`
     } else {
         e.target.textContent = `x`
     }
 })
+
+const smallScreen = document.querySelectorAll(".small-screen")
+
+smallScreen.forEach(el => () => {
+    el.addEventListener("click", (ev) => {
+        ev.target.className.style.color = "orange"
+        console.log("kuk small-screen ev targetclicked");
+    })
+})
+
+// document.querySelector("#menuIcon").addEventListener("click", (e) => {
+//     if (e.target.textContent == `x`) {
+//         e.target.textContent = `☰`
+//     } else {
+//         e.target.textContent = `x`
+//     }
+// })
