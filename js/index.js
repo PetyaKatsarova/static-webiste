c0037ab3164b51bfec617bba9d87ff59d9f5099d
PetyaKatsarova <pskpetya@gmail.com>
2024-04-20 17:50:40 +0200

    nav bar for mobile and tablet complete, pages addded

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const menuItems = document.getElementById("menuItems");
 const menuIcon  = document.getElementById("menuIcon");
 
+// toggle hamburger navigation menu for small screens
 menuIcon.addEventListener("click", (e)=> {
     var menuItems = e.target.nextElementSibling;
     if (menuItems.style.display === "none" || menuItems.style.display === "") {
@@ -17,19 +18,38 @@ menuIcon.addEventListener("click", (e)=> {
     }
 })
 
-const smallScreen = document.querySelectorAll(".small-screen")
+// for small screen: active nav link changes color and background
+// document.addEventListener("DOMContentLoaded", function() {
+//     const links = document.querySelectorAll("#menuItems li a");
 
-smallScreen.forEach(el => () => {
-    el.addEventListener("click", (ev) => {
-        ev.target.className.style.color = "orange"
-        console.log("kuk small-screen ev targetclicked");
-    })
-})
+//     links.forEach(link => {
+//         link.addEventListener("click", function() {
+//             links.forEach(el => el.classList.remove('active'));
+//             this.classList.add('active');
+//         });
+//     });
+// });
 
-// document.querySelector("#menuIcon").addEventListener("click", (e) => {
-//     if (e.target.textContent == `x`) {
-//         e.target.textContent = `☰`
-//     } else {
-//         e.target.textContent = `x`
-//     }
-// })
+
+
+// add/remove above/under line on click on nav link on bigger screen
+document.querySelectorAll("#menuItems li a").forEach(el => {
+    el.addEventListener("click", function() {
+        // Remove active class from all links
+        document.querySelectorAll("#menuItems li a").forEach(el => {
+            if (el.classList == 'active') {
+                el.classList.remove('active');
+            } else {
+                this.classList.add('active');
+            }
+            
+        });
+        // Add active class to the clicked link
+        // this.classList.add('active');
+    });
+});
+
+// Set the first link on the right as default active
+// document.querySelector("#menuItems li:first-child a").classList.add('active');
+
+// for small screen: on click on link color active link
